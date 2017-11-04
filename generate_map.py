13b5cdae0f5e6e32e44f76e536206d10a9eec91a
Yonatan Kogan <yonatan@tockhq.com>
2017-11-04 16:09:11 -0400

    Reorder puzzles, generate new maps, and add draft 11

--- generate_map.py
+++ generate_map.py
@@ -39,20 +39,20 @@ def to_array(in_file, out_file, order, index, apikey):
 if __name__ == '__main__':
   # Answer: Argentina (+54)
   to_array('data/billionaires.csv', './puzzle/1.html', 'KazakhstanMonaco', 1, 'AIzaSyCDB45ZVFNENiV2bUO92tzHO095stqd7F8')
-  # Answer: Ethiopia (+251)
-  to_array('data/reactors.csv', './puzzle/2.html', 'BrazilSwitzerlandArmenia', 2, 'AIzaSyD7L4w39LZ22obci_jIoZ5iT-DoccCtrw8')
-  # Answer: Iceland (+354)
-  to_array('data/survivor.csv', './puzzle/3.html', 'PanamaFijiPhilippines', 1, 'AIzaSyA0oD5trz09rmpopMLmHfyn-mtTbh3dsqk')
-  # Answer: Indonesia (+62)
-  to_array('data/un.csv', './puzzle/4.html', 'Cambodia', 2, 'AIzaSyDGWjCK3Q6MLTfnMinDULYkSUXTn5D8GR4') # Needs own key
-  # Answer: Morocco (+212)
-  to_array('data/oscars.csv', './puzzle/5.html', 'IranFrance', 3, 'AIzaSyA0oD5trz09rmpopMLmHfyn-mtTbh3dsqk') # Needs own key
-  # Answer: New Zealand (+64)
-  to_array('data/worlds50best.csv', './puzzle/6.html', 'SpainItaly', 7, 'AIzaSyDzevghJT_Wy7hwZzy5IB5bKfUBe6k14yI')
-  # Answer: Nicaragua (+505)
-  to_array('data/mountains.csv', './puzzle/7.html', 'ChinaBhutan', 4, 'AIzaSyDke9m-rn4eIK-v39ah-RliNTfp-Zw0UGg')
   # Answer: Denmark (+45)
-  to_array('data/letters.csv', './puzzle/8.html', 'UnitedKingdom', 3, 'AIzaSyA5pwIIpyL6u9kuOVs0i_KXitv65toze5c')
+  to_array('data/letters.csv', './puzzle/2.html', 'UnitedKingdom', 3, 'AIzaSyA5pwIIpyL6u9kuOVs0i_KXitv65toze5c')
+  # Answer: Ethiopia (+251)
+  to_array('data/reactors.csv', './puzzle/3.html', 'BrazilSwitzerlandArmenia', 2, 'AIzaSyD7L4w39LZ22obci_jIoZ5iT-DoccCtrw8')
+  # Answer: Iceland (+354)
+  to_array('data/survivor.csv', './puzzle/4.html', 'PanamaFijiPhilippines', 1, 'AIzaSyA0oD5trz09rmpopMLmHfyn-mtTbh3dsqk')
+  # Answer: Indonesia (+62)
+  to_array('data/un.csv', './puzzle/5.html', 'Cambodia', 2, 'AIzaSyDGWjCK3Q6MLTfnMinDULYkSUXTn5D8GR4') # Needs own key
+  # Answer: Morocco (+212)
+  to_array('data/oscars.csv', './puzzle/6.html', 'IranFrance', 3, 'AIzaSyA0oD5trz09rmpopMLmHfyn-mtTbh3dsqk') # Needs own key
+  # Answer: New Zealand (+64)
+  to_array('data/worlds50best.csv', './puzzle/7.html', 'SpainItaly', 7, 'AIzaSyDzevghJT_Wy7hwZzy5IB5bKfUBe6k14yI')
+  # Answer: Nicaragua (+505)
+  to_array('data/mountains.csv', './puzzle/8.html', 'ChinaBhutan', 4, 'AIzaSyDke9m-rn4eIK-v39ah-RliNTfp-Zw0UGg')
   # Answer: Pakistan (+92)
   to_array('data/timezones.csv', './puzzle/9.html', 'UnitedKingdomEcuador', 8, 'AIzaSyD2kUuTMcnK0Vh41G-MMpTDIzV4sk9oTCo')
   # Answer: Republic of the Congo (+242)
